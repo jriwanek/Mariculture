ab71fe6aeadd52d31d6880175afcf22697072e3e
Jasmine Iwanek <jasmine@iwanek.co.uk>
2015-09-21 20:52:44 -0700

    Allow smeltery to break down Titanium tool parts.

--- src/main/java/mariculture/plugins/tconstruct/TitaniumTools.java
+++ src/main/java/mariculture/plugins/tconstruct/TitaniumTools.java
@@ -74,6 +74,8 @@ public class TitaniumTools {
                 PatternBuilder.instance.registerFullMaterial(ingot, 2, "Titanium", new ItemStack(chunk, 1, titanium_id), new ItemStack(tool_rod, 1, titanium_id), titanium_id);
             }
         }
+        
+        addMelting(800);
 
         addTools();
 
@@ -109,6 +111,32 @@ public class TitaniumTools {
         PluginTConstruct.addPartCasting(new ItemStack(large_sword_blade, 1, id), TConstructRegistry.getItemStack("largeBladeCast"), titanium(INGOT * 8), delay);
         PluginTConstruct.addPartCasting(new ItemStack(hammer_head, 1, id), TConstructRegistry.getItemStack("hammerHeadCast"), titanium(INGOT * 8), delay);
         PluginTConstruct.addPartCasting(new ItemStack(full_guard, 1, id), TConstructRegistry.getItemStack("fullGuardCast"), titanium(INGOT * 3), delay);
+    }
+
+    public static void addMelting(int delay) {
+        // Melt Down Titanium
+        PluginTConstruct.addMelting(Core.metals, MetalMeta.TITANIUM_BLOCK, "tool_rod", getFluidStack("titanium", MetalRates.INGOT / 2), delay);
+        PluginTConstruct.addMelting(Core.metals, MetalMeta.TITANIUM_BLOCK, "pickaxe_head", getFluidStack("titanium", MetalRates.INGOT), delay);
+        PluginTConstruct.addMelting(Core.metals, MetalMeta.TITANIUM_BLOCK, "shovel_head", getFluidStack("titanium", MetalRates.INGOT), delay);
+        PluginTConstruct.addMelting(Core.metals, MetalMeta.TITANIUM_BLOCK, "axe_head", getFluidStack("titanium", MetalRates.INGOT), delay);
+        PluginTConstruct.addMelting(Core.metals, MetalMeta.TITANIUM_BLOCK, "sword_blade", getFluidStack("titanium", MetalRates.INGOT), delay);
+        PluginTConstruct.addMelting(Core.metals, MetalMeta.TITANIUM_BLOCK, "large_guard", getFluidStack("titanium", MetalRates.INGOT / 2), delay);
+        PluginTConstruct.addMelting(Core.metals, MetalMeta.TITANIUM_BLOCK, "hand_guard", getFluidStack("titanium", MetalRates.INGOT / 2), delay);
+        PluginTConstruct.addMelting(Core.metals, MetalMeta.TITANIUM_BLOCK, "crossbar", getFluidStack("titanium", MetalRates.INGOT / 2), delay);
+        PluginTConstruct.addMelting(Core.metals, MetalMeta.TITANIUM_BLOCK, "binding", getFluidStack("titanium", MetalRates.INGOT / 2), delay);
+        PluginTConstruct.addMelting(Core.metals, MetalMeta.TITANIUM_BLOCK, "frypan_head", getFluidStack("titanium", MetalRates.INGOT), delay);
+        PluginTConstruct.addMelting(Core.metals, MetalMeta.TITANIUM_BLOCK, "battle_sign_head", getFluidStack("titanium", MetalRates.INGOT), delay);
+        PluginTConstruct.addMelting(Core.metals, MetalMeta.TITANIUM_BLOCK, "knife_blade", getFluidStack("titanium", MetalRates.INGOT / 2), delay);
+        PluginTConstruct.addMelting(Core.metals, MetalMeta.TITANIUM_BLOCK, "chisel_head", getFluidStack("titanium", MetalRates.INGOT / 2), delay);
+        PluginTConstruct.addMelting(Core.metals, MetalMeta.TITANIUM_BLOCK, "tough_rod", getFluidStack("titanium", MetalRates.INGOT * 3), delay);
+        PluginTConstruct.addMelting(Core.metals, MetalMeta.TITANIUM_BLOCK, "tough_binding", getFluidStack("titanium", MetalRates.INGOT * 3, delay);
+        PluginTConstruct.addMelting(Core.metals, MetalMeta.TITANIUM_BLOCK, "large_plate", getFluidStack("titanium", MetalRates.INGOT * 8), delay);
+        PluginTConstruct.addMelting(Core.metals, MetalMeta.TITANIUM_BLOCK, "broad_axe_head", getFluidStack("titanium", MetalRates.INGOT * 8), delay);
+        PluginTConstruct.addMelting(Core.metals, MetalMeta.TITANIUM_BLOCK, "scythe_head", getFluidStack("titanium", MetalRates.INGOT * 8), delay);
+        PluginTConstruct.addMelting(Core.metals, MetalMeta.TITANIUM_BLOCK, "excavator_head", getFluidStack("titanium", MetalRates.INGOT * 8), delay);
+        PluginTConstruct.addMelting(Core.metals, MetalMeta.TITANIUM_BLOCK, "large_sword_blade", getFluidStack("titanium", MetalRates.INGOT * 8), delay);
+        PluginTConstruct.addMelting(Core.metals, MetalMeta.TITANIUM_BLOCK, "hammer_head", getFluidStack("titanium", MetalRates.INGOT * 8), delay);
+        PluginTConstruct.addMelting(Core.metals, MetalMeta.TITANIUM_BLOCK, "full_guard", getFluidStack("titanium", MetalRates.INGOT * 3, delay);
     }
 
     public static void addTools() {
